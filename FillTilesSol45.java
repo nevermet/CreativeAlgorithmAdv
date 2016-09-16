827eac8c2600c8c6d9d422ce83d931c71ebaa1c5
nevermet <seunghon7@gmail.com>
2016-09-16 19:10:57 +0900

    Add files via upload

---
+++ FillTilesSol45.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FillTilesSol45 {
+
+	public static int n;
+	public static int m;
+		
+	public static long f(int k) {
+		if (k<=1)
+			return 1%m;
+		else if (k%2>0)
+			return (f(k/2)*f(k/2+1)+2*f(k/2)*f(k/2-1))%m;
+		else
+			return (f(k/2)*f(k/2)+2*f(k/2-1)*f(k/2-1))%m;
+	}
+		
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		sc.close();
+			
+		System.out.println(f(n));
+			
+		return;
+	}
+
+}
