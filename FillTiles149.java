1bab5f2ec50a1702d17fb98a2bbed21045c12007
nevermet <seunghon7@gmail.com>
2017-02-02 12:18:46 +0900

    Add files via upload

---
+++ FillTiles149.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FillTiles149 {
+
+	public static int n, m;
+	public static int[] DT = new int[100001];
+	
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		sc.close();
+		
+		DT[1]=1;
+		DT[2]=3;
+		DT[3]=5;
+		
+		for (int i=4; i <=n; i++){
+			if (i%2==0){
+				DT[i]=(DT[i/2]*DT[i/2]+DT[i/2-1]*DT[i/2-1]*2)%m;
+			}
+			else{
+				DT[i]=(DT[i/2]*DT[i/2+1]+DT[i/2]*DT[i/2-1]*2)%m;
+			}
+		}
+		System.out.println(DT[n]);
+		return;
+	}
+}
